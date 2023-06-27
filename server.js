6b14758989a78802317ee2ad647d89561775e05b
Ryan Cook <ryancook423@gmail.com>
2023-06-27 15:40:01 -0400

    feat: Added SSL option. Added self signed cert generate script.

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+var fs = require('fs');
+
 // Listen on a specific host via the HOST environment variable
 var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8080;
+// Wether or not to verify SSL certs with the VERIFY_SSL environment varable
+var verifySSL = process.env.VERIFY_SSL || true;
+
+if (verifySSL === 'true') {
+  verifySSL = true;
+} else if (verifySSL === 'false') {
+  verifySSL = false;
+}
+
+// The filepath to the SSL key (for https)
+var key = process.env.KEY;
+// The filepath to the SSL cert (for https)
+var cert = process.env.CERT;
 
 // Grab the blacklist from the command-line so that we can update the blacklist without deploying
 // again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
@@ -16,6 +31,15 @@ function parseEnvList(env) {
   return env.split(',');
 }
 
+var httpsOptions;
+
+if (key && cert) {
+  httpsOptions = {
+    key: fs.readFileSync(key),
+    cert: fs.readFileSync(cert),
+  }
+}
+
 // Set up rate-limiting to avoid abuse of the public CORS Anywhere server.
 var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
 
@@ -23,27 +47,15 @@ var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
-  requireHeader: ['origin', 'x-requested-with'],
+  requireHeader: [],
   checkRateLimit: checkRateLimit,
-  removeHeaders: [
-    'cookie',
-    'cookie2',
-    // Strip Heroku-specific headers
-    'x-request-start',
-    'x-request-id',
-    'via',
-    'connect-time',
-    'total-route-time',
-    // Other Heroku added debug headers
-    // 'x-forwarded-for',
-    // 'x-forwarded-proto',
-    // 'x-forwarded-port',
-  ],
+  removeHeaders: [ 'cookie', 'cookie2' ],
   redirectSameOrigin: true,
   httpProxyOptions: {
-    // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
-    xfwd: false,
+    secure: false
   },
+  httpsOptions,
 }).listen(port, host, function() {
-  console.log('Running CORS Anywhere on ' + host + ':' + port);
+  const protocol = key && cert ? 'https' : 'http';
+  console.log(`Running CORS Anywhere on ${protocol}://${host}:${port}`);
 });
